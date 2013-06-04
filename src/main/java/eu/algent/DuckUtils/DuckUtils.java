ba907495bc9b3c81d09b893552d84286ccaa9fb0
Algent <algent@gmail.com>
2013-06-04 02:01:12 +0200

    Egit trolled me.

---
+++ src/main/java/eu/algent/DuckUtils/DuckUtils.java
@@ -0,0 +1,16 @@
+package eu.algent.DuckUtils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DuckUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO Auto-generated method stub
+    }
+    
+    @Override
+    public void onDisable() {
+        // TODO Auto-generated method stub
+    }
+}
